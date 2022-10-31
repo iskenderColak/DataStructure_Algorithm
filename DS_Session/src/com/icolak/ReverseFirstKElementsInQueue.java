eeaeef3e32e9dae5f0351a0ecfdb0695f79b6985
iskenderColak <iclk84@gmail.com>
2022-10-31 10:29:08 +0300

    Reverse first k elements in queue

--- DS_Session/src/com/icolak/ReverseFirstKElementsInQueue.java
+++ DS_Session/src/com/icolak/ReverseFirstKElementsInQueue.java
@@ -38,5 +38,10 @@ public class ReverseFirstKElementsInQueue {
         }
         return queue;
     }
-
 }
+/*
+Given an integer K and a queue of integers,
+write code to reverse the order of the first K elements of the queue.
+-Input: Q = [10, 20, 30, 40, 50], K = 3
+-Output: Q = [30, 20, 10, 40, 50]
+*/
