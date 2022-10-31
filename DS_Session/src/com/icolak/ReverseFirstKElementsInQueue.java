fa7bfb9f2336400b90f83544e280221a3c2f0cd7
iskenderColak <iclk84@gmail.com>
2022-10-31 10:25:43 +0300

    Reverse first k elements in queue

---
+++ DS_Session/src/com/icolak/ReverseFirstKElementsInQueue.java
@@ -0,0 +1,42 @@
+package com.icolak;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseFirstKElementsInQueue {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.addAll(Arrays.asList(10,20,30,40,50));
+        int k = 3;
+        System.out.println(reverseFirstKElements(queue, k));
+
+    }
+
+    private static Queue<Integer> reverseFirstKElements(Queue<Integer> queue, int k) {
+        int size = queue.size();
+
+        // create a stack
+        Stack<Integer> stack = new Stack<>();
+
+        // push first K elements into stack
+        for (int i = 0; i < k; i++) {
+            stack.push(queue.poll());
+        }
+
+        // add elements back into the queue
+        while(!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+
+        // poll and add (size of queue - k) times
+        for (int i = 0; i < (size - k); i++) {
+            queue.add(queue.poll());
+        }
+        return queue;
+    }
+
+}
